ae7e4b05869974c95d5a366a9f81e6e68e96f491
zhangjiluo.com <zhangjiluo.com@gmail.com>
2023-10-18 21:15:21 +0800

    feat: prettier

---
+++ packages/prettier/src/index.ts
@@ -0,0 +1,33 @@
+import { type Config } from 'prettier'
+export { version } from '../package.json'
+
+const config: Config = {
+  // 不添加非必要分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 修改 Markdown 样式
+  proseWrap: 'never',
+  // 尽可能省略箭头函数括号
+  arrowParens: 'avoid',
+  // 忽略 HTML 中空白字符
+  htmlWhitespaceSensitivity: 'ignore',
+  overrides: [
+    {
+      files: ['*.css', '*.less', '*.scss', '*.sass'],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
+}
+
+export const {
+  semi,
+  singleQuote,
+  proseWrap,
+  htmlWhitespaceSensitivity,
+  overrides,
+} = config
+
+export default config
